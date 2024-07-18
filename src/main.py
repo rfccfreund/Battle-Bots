ecbd0ab2a9aca1d0235650afc1ec6efad60e0aa1
rfccfreund <rmfreund@gmail.com>
2024-07-18 00:09:05 -0500

    added player 2 to allow policy comparison

--- src/main.py
+++ src/main.py
@@ -6,34 +6,42 @@ import numpy as np
 # game object takes a gameMap, a list of nodes, and a policy
 
 
-test_bot = rl_bot.RL_Bot(gb.nodes, .33)
+player1 = rl_bot.RL_Bot(gb.nodes, .33)
+player2 = rl_bot.RL_Bot(gb.nodes, .5)
+
+players = [player1, player2]
 
 
 # play game function takes a bot and game object and runs the game
-def play_game(game, bot):
+def play_game(game, bots):
     bot_choice = gb.A
-    while game.game_over():
-        move = bot.step(game.find_bot_move(bot_choice))
-        game.update_moves(move)
-        bot.update_values(move.score(), move)
-        bot_choice = move
+    for bot in bots:
+        while game.game_over():
+            move = bot.step(game.find_bot_move(bot_choice))
+            game.update_moves(move)
+            bot.update_values(move.score(), move)
+            bot_choice = move
 
-        game.next_turn()
+            game.next_turn()
 
-    bot.update_rewards(game.num_moves())
-    bot.expected_values()
-    game.game_reset()
+        bot.update_rewards(game.num_moves())
+        bot.expected_values()
+        game.game_reset()
 
 
-# run_num returns the number of times the game has run. This alllows us to alter the number of runs
+# run_num returns the number of times the game has run. This allows us to alter the number of runs
 if __name__ == '__main__':
-    while test_bot.run_num() < 150:
-        play_game(gb.firstGame, test_bot)
+    while players[-1].run_num() < 150:
+        play_game(gb.firstGame, players)
 
-    ypoints = np.array(test_bot.all_scores())
+    player1_score = np.array(players[0].all_scores())
+    player2_score = np.array(players[1].all_scores())
 
-    plt.plot(ypoints, marker=".", linestyle='None')
+    plt.plot(player1_score, marker=".", linestyle='None')
+    plt.plot(player2_score, marker=".", linestyle='None')
     plt.show()
 
     # after the strategy is defined by the loop we set the policy to one. Returns a list of best moves
-    test_bot.strategy(gb.firstGame, 5, gb.A)
+    players[0].strategy(gb.firstGame, 5, gb.A)
+    print("\n")
+    players[1].strategy(gb.firstGame, 5, gb.A)
