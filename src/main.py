35b600b8bd281f8dcd02b54350595bb4347ab02f
rfccfreund <rmfreund@gmail.com>
2024-07-28 19:45:36 -0500

    seperated out player logic from main loop

--- src/main.py
+++ src/main.py
@@ -1,54 +1,24 @@
 import random
-
 import gameboard as gb
-import rl_bot
 import matplotlib.pyplot as plt
 import numpy as np
+import engine
+import players
 
 # game object takes a gameMap, a list of nodes, and a policy
-
 random.seed(42)
 
-# create player objects with varying exploration coeeficents
-player1 = rl_bot.RL_Bot(gb.nodes_m, .25)
-player2 = rl_bot.RL_Bot(gb.nodes_m, .5)
-player3 = rl_bot.RL_Bot(gb.nodes_m, .75)
-
-players = [player1, player2, player3]
-
-
-# play game function takes a bot and game object and runs the game
-def play_game(game, bots):
-
-    for bot in bots:
-        bot_loc = gb.Start
-        while game.game_over():
-            move = bot.step(game.find_bot_move(bot_loc))
-            game.update_moves(move)
-            bot.score_move(move.score())
-            bot.add_move(move)
-
-            bot_loc = move
-
-            game.next_turn()
-
-        bot.add_game_score()
-        bot.update_rewards()
-        bot.update_policy()
-        bot.update_explore_co()
-        bot.expected_values()
-        bot.player_cleanup()
-        game.game_reset()
-
+exploration_co = [.25, .5, .75]
+agents = players.make_players(gb.nodes_m, exploration_co, 3)
 
 # run_num returns the number of times the game has run. This allows us to alter the number of runs
 if __name__ == '__main__':
-    while len(players[2].all_scores()) < 150:
-        play_game(gb.secondGame, players)
+    while len(agents[2].all_scores()) < 150:
+        engine.play_game(gb.secondGame, agents)
 
-    player1_score = np.array(players[0].all_scores())
-    player2_score = np.array(players[1].all_scores())
-    player3_score = np.array(players[2].all_scores())
+    player1_score = np.array(agents[0].all_scores())
+    player2_score = np.array(agents[1].all_scores())
+    player3_score = np.array(agents[2].all_scores())
 
     plt.plot(player1_score, marker=".", linestyle='None')
     plt.plot(player2_score, marker="+", linestyle='None')
